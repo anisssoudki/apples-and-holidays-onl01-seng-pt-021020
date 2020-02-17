3a49009a22b358170d817fc218f7f7c45369566b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-17 23:50:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -6,7 +6,9 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
 
-holiday_supplies.each do |holiday_hash, supply|
+holiday_hash.each do |supply, holiday|
+  puts supply
+  puts holiday
   
   
 
